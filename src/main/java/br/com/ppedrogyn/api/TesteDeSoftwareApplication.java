109859f73e2bd635c19633d7a567571fd732c9fe
Pedro Paulo <pedro.paulods@hotmail.com>
2017-11-21 06:01:05 -0200

    Commit inicial do projeto.

---
+++ src/main/java/br/com/ppedrogyn/api/TesteDeSoftwareApplication.java
@@ -0,0 +1,12 @@
+package br.com.ppedrogyn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDeSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDeSoftwareApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/ppedrogyn/api/TesteDeSoftwareApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ppedrogyn.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TesteDeSoftwareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
